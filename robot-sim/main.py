bc0498f8a5592fa87bbe11f59bc06f99c8ede084
Iain Butler <i@iainbutler.co.uk>
2015-07-29 14:59:21 +0100

    Moved to nearest token, knows type collected, knows how to get back

--- robot-sim/main.py
+++ robot-sim/main.py
@@ -4,31 +4,47 @@ from random import random
 import time
 
 SEARCHING, DRIVING = range(2)
+movement = []
+MARKER_TYPE = 0
 
 R = Robot()
 
 token_filter = lambda m: m.info.marker_type in (MARKER_TOKEN_GOLD, MARKER_TOKEN_SILVER)
+token_filter_g = lambda m: m.info.marker_type in (MARKER_TOKEN_GOLD)
+token_filter_s = lambda m: m.info.marker_type in (MARKER_TOKEN_SLIVER)
+
 
 def drive(speed, seconds):
-    R.motors[0].m0.power = speed
-    R.motors[0].m1.power = speed
-    time.sleep(seconds)
-    R.motors[0].m0.power = 0
-    R.motors[0].m1.power = 0
+	R.motors[0].m0.power = speed
+	R.motors[0].m1.power = speed
+	time.sleep(seconds)
+	R.motors[0].m0.power = 0
+	R.motors[0].m1.power = 0
+	addMove({'m':"F",'sp':speed, 'sec':seconds})
 
 def turn(speed, seconds):
-    R.motors[0].m0.power = speed
-    R.motors[0].m1.power = -speed
-    time.sleep(seconds)
-    R.motors[0].m0.power = 0
-    R.motors[0].m1.power = 0
+	R.motors[0].m0.power = speed
+	R.motors[0].m1.power = -speed
+	time.sleep(seconds)
+	R.motors[0].m0.power = 0
+	R.motors[0].m1.power = 0
+	addMove({'m':"t",'sp':speed, 'sec':seconds})
 
 def getMarkers():
-	return filter(token_filter, R.see())
+	if MARKER_TYPE == 0:
+		return filter(token_filter, R.see())
+	if MARKER_TYPE == "token_silver":
+		return filter(token_filter_s, R.see())
+	elif MARKER_TYPE == "token_gold":
+		return filter(token_filter_g, R.see())
+		
 
 
 
 def nearestMarker(markers):
+	'''
+	Find the nearest marker
+	'''
 	minDist = markers[0].centre.polar.length
 	minMarker = 0
 	i = 0
@@ -41,78 +57,44 @@ def nearestMarker(markers):
 #	print "Heading towards marker", markers[minMarker].info.code
 	return markers[minMarker]
 
-def rotateToNearest():
+
+def rotateToNearest(speed,sec,err):
 	m = getMarkers()
-	print nearestMarker(m).centre.polar.rot_y
-	if nearestMarker(m).centre.polar.rot_y > 0.1:
-		while (nearestMarker(m).centre.polar.rot_y > 0.1):
+	if nearestMarker(m).centre.polar.rot_y > err:
+		while (nearestMarker(m).centre.polar.rot_y > err):
 			m = getMarkers()
 			print nearestMarker(m).centre.polar.rot_y
-			turn(25,0.01)
-	elif nearestMarker(m).centre.polar.rot_y < -0.1:
-		while (nearestMarker(m).centre.polar.rot_y < -0.1):
+			turn(speed,sec)
+	elif nearestMarker(m).centre.polar.rot_y < -err:
+		while (nearestMarker(m).centre.polar.rot_y < -err):
 			m = getMarkers()
 			print nearestMarker(m).centre.polar.rot_y
-			turn(-25,0.01)
+			turn(-speed,sec)
 	return nearestMarker(m)
 
+def collect():
+	'''
+	Now token is in range pick it up
+	'''
+	global MARKER_TYPE
+	MARKER_TYPE = rotateToNearest(5,0.01,0.05)
+	print "Looking for", MARKER_TYPE.info.marker_type
+	drive(100,0.1)
+	
+	
+def addMove(move):
+	movement.append(move)
+	
 
-print nearestMarker(getMarkers())
+
 while nearestMarker(getMarkers()).dist > 0.5:
-	rotateToNearest()
-	print "F", nearestMarker(getMarkers()).dist
+	rotateToNearest(25,0.01,0.5)
+	print "F to",nearestMarker(getMarkers()).info.code, " - Distance", nearestMarker(getMarkers()).dist
 	drive(100,0.5)
-#rotateToNearest()
+print "Found marker"
+collect()
+print movement
 
+
+print "END"	
 exit()
-'''
-
-state = SEARCHING
-
-while True:
-    if state == SEARCHING:
-        print "Searching..."
-        tokens = filter(token_filter, R.see())
-        if len(tokens) > 0:
-            m = tokens[0]
-            print "Token sighted. {0} is {1}m away, bearing {2} degrees." \
-                  .format(m.info.offset, m.dist, m.rot_y)
-            state = DRIVING
-
-        else:
-            print "Can't see anything."
-            turn(25, random())
-            time.sleep(0.2)
-
-    elif state == DRIVING:
-        print "Aligning..."
-        tokens = filter(token_filter, R.see())
-        if len(tokens) == 0:
-            state = SEARCHING
-
-        else:
-            m = tokens[0]
-            if m.dist < 0.4:
-                print "Found it!"
-                if R.grab():
-                    print "Gotcha!"
-                    turn(50, 0.5)
-                    drive(50, 1)
-                    R.release()
-                    drive(-50, 0.5)
-                else:
-                    print "Aww, I'm not close enough."
-                exit()
-
-            elif -15 <= m.rot_y <= 15:
-                print "Ah, that'll do."
-                drive(50, 0.5)
-
-            elif m.rot_y < -15:
-                print "Left a bit..."
-                turn(-12.5, 0.5)
-
-            elif m.rot_y > 15:
-                print "Right a bit..."
-                turn (12.5, 0.5)
-'''
